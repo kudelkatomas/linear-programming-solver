8f50c1a24900c27aa9a9465ed338eb3b7e0025c0
Tomas Kudelka <tom.kudelka3@gmail.com>
2026-04-30 13:52:56 +0200

    Update gitignore, remove MyLib

--- src/LPSolver/Simplex.hs
+++ src/LPSolver/Simplex.hs
@@ -1,1 +1,12 @@
 module LPSolver.Simplex where
+
+import Data.Matrix (Matrix, fromLists)
+
+mat :: Matrix Integer
+mat =
+  fromLists
+    [ [1, 1, 3, 30],
+      [2, 2, 5, 24],
+      [4, 1, 2, 36],
+      [-3, -1, -2, 0]
+    ]
