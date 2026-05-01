fc3ba40180419ada055579a1df8263db5d58a328
Tomas Kudelka <tom.kudelka3@gmail.com>
2026-05-01 19:34:51 +0200

    Implement basic version of Simplex

--- src/LPSolver/Simplex.hs
+++ src/LPSolver/Simplex.hs
@@ -1,8 +1,22 @@
 module LPSolver.Simplex where
 
-import Data.Matrix (Matrix, fromList, fromLists, identity, nrows, (<->), (<|>))
+import Data.Matrix
+  ( combineRows,
+    fromList,
+    fromLists,
+    getCol,
+    getElem,
+    identity,
+    nrows,
+    scaleRow,
+    (<->),
+    (<|>),
+  )
 import Data.Ratio ((%))
-import LPSolver.Types (LPInstance (..))
+import Data.Vector (maxIndex, minIndex, zipWith, (!))
+import LPSolver.Types (LPInstance (..), Tableau, getLastColumn, getLastRow)
+
+------------------------------------------------------------------------------------------
 
 inst :: LPInstance
 inst =
@@ -16,19 +30,105 @@ inst =
       vecC = [-3, -1, -2]
     }
 
-instanceMatrix :: [[Integer]]
-instanceMatrix =
-  [ [1, 1, 3, 30],
-    [2, 2, 5, 24],
-    [4, 1, 2, 36],
-    [-3, -1, -2, 0]
-  ]
-
 -- | Combines matA, vecB, vecC and an identity matrix to build the tableau
-buildTableau :: LPInstance -> Matrix Rational
-buildTableau (LPInstance a b c) =
-  (% 1) <$> (aWithC <|> identityMat) <|> bWithZero
+buildTableau :: LPInstance -> Tableau
+buildTableau (LPInstance a b c) = (% 1) <$> aIdentityC <|> bWithZero
   where
-    aWithC = fromLists a <-> fromList 1 (length c) c
-    identityMat = identity (nrows aWithC)
+    aMat = fromLists a
+    aNRows = nrows aMat
+    cWithZeros = c ++ replicate aNRows 0
     bWithZero = fromList (length b + 1) 1 (b ++ [0])
+    aIdentityC = (aMat <|> identity aNRows) <-> fromList 1 (length c + aNRows) cWithZeros
+
+{-
+>>> buildTableau inst
+┌                                                                ┐
+│    1 % 1    1 % 1    3 % 1    1 % 1    0 % 1    0 % 1   30 % 1 │
+│    2 % 1    2 % 1    5 % 1    0 % 1    1 % 1    0 % 1   24 % 1 │
+│    4 % 1    1 % 1    2 % 1    0 % 1    0 % 1    1 % 1   36 % 1 │
+│ (-3) % 1 (-1) % 1 (-2) % 1    0 % 1    0 % 1    0 % 1    0 % 1 │
+└                                                                ┘
+-}
+
+------------------------------------------------------------------------------------------
+
+findPivotColumnIndex :: Tableau -> Maybe Int
+findPivotColumnIndex tbl = if lastRow ! minIdx < 0 then Just (minIdx + 1) else Nothing
+  where
+    lastRow = getLastRow tbl
+    minIdx = minIndex lastRow
+
+findPivotRowIndex :: Tableau -> Int -> Maybe Int
+findPivotRowIndex tbl pivotColIdx = ratios ! minIdx >> Just (minIdx + 1)
+  where
+    -- Nothing < Just _, therefore ratios multiplied by -1 and maxIndex
+    ratiosDiv :: Rational -> Rational -> Maybe Rational
+    ratiosDiv n d = if d > 0 then Just (n / (-d)) else Nothing
+
+    ratios = Data.Vector.zipWith ratiosDiv (getLastColumn tbl) (getCol pivotColIdx tbl)
+    minIdx = maxIndex ratios
+
+-- | Finds pivot coordinates (column, row) indexed from 1
+findPivotPos :: Tableau -> Maybe (Int, Int)
+findPivotPos tbl =
+  do
+    col <- findPivotColumnIndex tbl
+    row <- findPivotRowIndex tbl col
+    return (col, row)
+
+{-
+>>> findPivotPos $ buildTableau inst
+Just (1,3)
+-}
+
+------------------------------------------------------------------------------------------
+
+-- | Pivot around the pivot element
+updateTableau :: Tableau -> (Int, Int) -> Tableau
+updateTableau tbl (col, row) = foldl updateRow scaledTbl targetRows
+  where
+    targetRows = [i | i <- [1 .. nrows tbl], i /= row]
+    scaledTbl =
+      let pivot = getElem row col tbl
+       in scaleRow (1 / pivot) row tbl
+
+    updateRow :: Tableau -> Int -> Tableau
+    updateRow tbl' rowIdx = combineRows rowIdx (-(getElem rowIdx col tbl')) row tbl'
+
+updateTableauTest :: Maybe Tableau
+updateTableauTest =
+  do
+    let tbl = buildTableau inst
+    pos <- findPivotPos tbl
+    return $ updateTableau tbl pos
+
+{-
+>>> updateTableauTest
+Just ┌                                                                ┐
+│    0 % 1    3 % 4    5 % 2    1 % 1    0 % 1 (-1) % 4   21 % 1 │
+│    0 % 1    3 % 2    4 % 1    0 % 1    1 % 1 (-1) % 2    6 % 1 │
+│    1 % 1    1 % 4    1 % 2    0 % 1    0 % 1    1 % 4    9 % 1 │
+│    0 % 1 (-1) % 4 (-1) % 2    0 % 1    0 % 1    3 % 4   27 % 1 │
+└                                                                ┘
+-}
+
+------------------------------------------------------------------------------------------
+
+simplex :: LPInstance -> Tableau
+simplex ins = helper $ buildTableau ins
+  where
+    helper :: Tableau -> Tableau
+    helper tbl =
+      case findPivotPos tbl of
+        Nothing -> tbl
+        Just pivotPos -> helper $ updateTableau tbl pivotPos
+
+{-
+>>> simplex inst
+┌                                                                ┐
+│    0 % 1    0 % 1    1 % 2    1 % 1 (-1) % 2    0 % 1   18 % 1 │
+│    0 % 1    1 % 1    8 % 3    0 % 1    2 % 3 (-1) % 3    4 % 1 │
+│    1 % 1    0 % 1 (-1) % 6    0 % 1 (-1) % 6    1 % 3    8 % 1 │
+│    0 % 1    0 % 1    1 % 6    0 % 1    1 % 6    2 % 3   28 % 1 │
+└                                                                ┘
+-}
--- src/LPSolver/Types.hs
+++ src/LPSolver/Types.hs
@@ -1,8 +1,29 @@
 module LPSolver.Types where
 
+import Data.Matrix (Matrix, getCol, getRow, ncols, nrows)
+import Data.Vector (Vector)
+
+------------------------------------------------------------------------------------------
+
 -- | Linear programming problem instance
 data LPInstance = LPInstance
   { matA :: [[Integer]],
     vecB :: [Integer],
     vecC :: [Integer]
   }
+
+------------------------------------------------------------------------------------------
+
+-- | Simplex algorithm tableau
+type Tableau = Matrix Rational
+
+-- | Simplex algorithm tableau row or column
+data TableauVector = Row (Vector Rational) | Column (Vector Rational)
+
+getLastColumn :: Tableau -> Vector Rational
+getLastColumn tbl = getCol (ncols tbl) tbl
+
+getLastRow :: Tableau -> Vector Rational
+getLastRow tbl = getRow (nrows tbl) tbl
+
+------------------------------------------------------------------------------------------
