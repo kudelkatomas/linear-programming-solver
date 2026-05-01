6c757bcf200fce35e0acbc7c2ddfedf93ee758e6
Tomas Kudelka <tom.kudelka3@gmail.com>
2026-05-01 10:36:13 +0200

    Add LPInstance type and basic functions

--- src/LPSolver/Simplex.hs
+++ src/LPSolver/Simplex.hs
@@ -1,12 +1,34 @@
 module LPSolver.Simplex where
 
-import Data.Matrix (Matrix, fromLists)
+import Data.Matrix (Matrix, fromList, fromLists, identity, nrows, (<->), (<|>))
+import Data.Ratio ((%))
+import LPSolver.Types (LPInstance (..))
 
-mat :: Matrix Integer
-mat =
-  fromLists
-    [ [1, 1, 3, 30],
-      [2, 2, 5, 24],
-      [4, 1, 2, 36],
-      [-3, -1, -2, 0]
-    ]
+inst :: LPInstance
+inst =
+  LPInstance
+    { matA =
+        [ [1, 1, 3],
+          [2, 2, 5],
+          [4, 1, 2]
+        ],
+      vecB = [30, 24, 36],
+      vecC = [-3, -1, -2]
+    }
+
+instanceMatrix :: [[Integer]]
+instanceMatrix =
+  [ [1, 1, 3, 30],
+    [2, 2, 5, 24],
+    [4, 1, 2, 36],
+    [-3, -1, -2, 0]
+  ]
+
+-- | Combines matA, vecB, vecC and an identity matrix to build the tableau
+buildTableau :: LPInstance -> Matrix Rational
+buildTableau (LPInstance a b c) =
+  (% 1) <$> (aWithC <|> identityMat) <|> bWithZero
+  where
+    aWithC = fromLists a <-> fromList 1 (length c) c
+    identityMat = identity (nrows aWithC)
+    bWithZero = fromList (length b + 1) 1 (b ++ [0])
--- src/LPSolver/Types.hs
+++ src/LPSolver/Types.hs
@@ -1,1 +1,8 @@
 module LPSolver.Types where
+
+-- | Linear programming problem instance
+data LPInstance = LPInstance
+  { matA :: [[Integer]],
+    vecB :: [Integer],
+    vecC :: [Integer]
+  }
